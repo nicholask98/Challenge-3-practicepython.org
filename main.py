8205a6d5dea557b06502b6105cf1f6e8e6abd0d4
nicholask98 <72052647+nicholask98@users.noreply.github.com>
2021-02-02 12:33:30 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# Base Challenge and Extra #1 and 3:
+# -----------------------------------------
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = []
+count = 0
+
+user_num = int(input('Enter a number:\n'))
+
+while count < len(a):
+    if a[count] < user_num:
+        b.append(a[count])
+    count += 1
+
+print('List a = {}'.format(a))
+print('Items from list a that are smaller than {} are {}'.format(user_num, b))
+# -----------------------------------------
